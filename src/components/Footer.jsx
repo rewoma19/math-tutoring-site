92b6afe055372bcf9f99175db329a881fc2b2cd8
Raphael <rewoma19@gmail.com>
2025-08-20 22:39:23 -0600

    Move HTML content into components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer>
+      <a target="_blank" href="https://www.linkedin.com/in/jamezmcarthur/">
+        <img
+          src="https://media.licdn.com/dms/image/v2/C5603AQG6Q4W3kWANtw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1630988413596?e=1752105600&v=beta&t=-hYSuomF1nMSIxNHoC8u6BbEYOQeOQbFVYQDD2GcD0s"
+          alt="pfp"
+        />
+        <p>LinkedIn</p>
+        <i className="fa-brands fa-linkedin-in"></i>
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <h1>Auckland Maths Tutoring</h1>
+      <button>Book a session</button>
+      <div>
+        <button>Book a session</button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
