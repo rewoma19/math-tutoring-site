8aedc66bf40af2fbe73df54e93527088bb139d72
Raphael <rewoma19@gmail.com>
2025-08-20 23:03:38 -0600

    Add fanta-css library and style Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
 function Header() {
   return (
-    <header>
-      <h1>Auckland Maths Tutoring</h1>
-      <button>Book a session</button>
-      <div>
-        <button>Book a session</button>
+    <header className="p-8">
+      <div className="flex items-center justify-between gap-8 max-w-[1000px] mx-auto">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl">
+          Auckland Maths Tutoring
+        </h1>
+        <button className="card-button-secondary px-4 py-2 rounded-full whitespace-nowrap opacity-0 pointer-events-none">
+          Book a session
+        </button>
+        <div className="fixed top-8 right-8">
+          <button className="card-button-secondary px-4 py-2 rounded-full whitespace-nowrap">
+            Book a session
+          </button>
+        </div>
       </div>
     </header>
   );
